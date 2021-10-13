e1105bd0002eaf221e6d5f3fa33646a4782de027
wuhantao <wuhantao1216@gmail.com>
2021-10-13 22:11:23 +0800

    add ts and babel

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     vue app of mine112333wew testfdf ggg
     <comp />
+    <comp2 />
   </div>
 </template>
 
 <script>
 import comp from './component.vue';
+import comp2 from './comp2.ts';
 export default {
   name: 'App',
   components: {
-    comp
+    comp,
+    comp2
   }
 };
 </script>

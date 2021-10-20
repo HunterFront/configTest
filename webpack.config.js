603ce17853aa28c98b3e8ea721d263781f11ccd7
wuht08 <wuht08@vanke.com>
2021-10-20 18:36:08 +0800

    add happy pack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const HappyPack = require('happypack');
 
 module.exports = {
   context: path.resolve(),
@@ -67,10 +68,22 @@ module.exports = {
     new HtmlWebpackPlugin({ template: './src/index.html' }),
     // new MiniCssExtractPlugin(),
     new CleanWebpackPlugin(),
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
     // new ESLintPlugin({
     //   extensions: ['js', 'vue', 'ts']
     // })
+    new HappyPack({
+      // 用唯一的标识符 id 来代表当前的 HappyPack 是用来处理一类特定的文件
+      id: 'babel',
+      // 如何处理 .js 文件，用法和 Loader 配置中一样
+      loaders: ['babel-loader?cacheDirectory']
+      // ... 其它配置项
+    }),
+    new HappyPack({
+      id: 'css',
+      // 如何处理 .css 文件，用法和 Loader 配置中一样
+      loaders: ['css-loader']
+    })
   ],
   target: ['web', 'es5'],
 

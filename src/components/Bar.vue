4ee75793a95a7a4d8d380a20938665607cc52439
wuht08 <wuht08@vanke.com>
2022-01-14 16:07:33 +0800

    add jsx support

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -1,13 +1,28 @@
-<template>
-  <div>bar</div>
-</template>
-
 <script>
 export default {
+  data() {
+    return {
+      message: 'jsx!'
+    };
+  },
+  render() {
+    const inputAttrs = {
+      type: 'email',
+      placeholder: 'Enter your email jsx'
+    };
 
-}
+    return (
+      <div
+        {...{
+          attrs: inputAttrs,
+          domProps: {
+            innerHTML: 'baz'
+          }
+        }}
+      ></div>
+    );
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
